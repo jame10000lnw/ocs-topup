b058f020f37a54f5d44c81a8d36504e0bc1e6a22
LION-HACK <33150334+lnwseed@users.noreply.github.com>
2018-07-31 08:29:25 +0700

    Update port.sh

--- port.sh
+++ port.sh
@@ -158,8 +158,7 @@ apt-get -y install zip unzip
 
 cd /home/vps/public_html
 
-#wget https://github.com/rasta-team/Full-OCS/raw/master/panelocs.zip
-wget https://raw.githubusercontent.com/lnwseed/ocs-topup/master/panelocs.zip
+wget https://github.com/rasta-team/Full-OCS/raw/master/panelocs.zip
 
 mv panelocs.zip LTEOCS.zip
 
